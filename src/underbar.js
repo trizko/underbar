dc4fffc0d614f443a47592f533d079fe6a71ee9f
Tony Rizko <tony@rizkocircle.com>
2014-09-16 16:24:07 -0700

    completed _.last() function

--- src/underbar.js
+++ src/underbar.js
@@ -39,6 +39,8 @@ var _ = {};
   // Like first, but for the last elements. If n is undefined, return just the
   // last element.
   _.last = function(array, n) {
+    array = array.reverse()
+    return n === undefined ? array[0] : array.slice(0, n).reverse();
   };
 
   // Call iterator(value, key, collection) for each element of collection.
