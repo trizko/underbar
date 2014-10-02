c292d40c96030a6c9bf62c3da93afae64308d485
Tony Rizko <tony@rizkocircle.com>
2014-10-01 18:06:59 -0700

    completed _.some() function

--- src/underbar.js
+++ src/underbar.js
@@ -199,6 +199,10 @@ var _ = {};
   // provided, provide a default one
   _.some = function(collection, iterator) {
     // TIP: There's a very clever way to re-use every() here.
+    iterator = iterator || function (item) {return Boolean(item);};
+    return !_.every(collection, function(item){
+      return !iterator(item);
+    });
   };
 
 
